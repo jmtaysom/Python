1b74a653e71bbcd64fc610d63654ed02801cadcd
jmtaysom <jmtaysom@users.noreply.github.com>
2015-12-17 12:14:30 -0500

    fixed prime generator

--- Euler/Project003.py
+++ Euler/Project003.py
@@ -7,22 +7,33 @@ Created on Thurs Dec 17  09:17:35 2015
 from __future__ import division
 from math import sqrt
 
+
 def primes_gen():
+    counter = 0
     primes = [2,3,5]
     n = 7
     increment = [4,2,4,2,4,6,2,6]
     
     while True:
-        for i in primes[3:int(sqrt(n))]:
-            if n % i == 0:
-                break
+        if counter == 0:
+            yield 2
+            counter += 1
+        elif counter == 1:
+            yield 3
+            counter += 1
+        elif counter == 2:
+            yield 5
+            counter += 1
         else:
-            primes.append(n)
-            yield primes.pop(0)
-        a = increment.pop(0)
-        increment.append(a)
-        n += a
-    
+            for i in primes[3:int(sqrt(n))]:
+                if n % i == 0:
+                    break
+            else:
+                primes.append(n)
+                yield primes[-1]
+            a = increment.pop(0)
+            increment.append(a)
+            n += a
 
 
 def prime_factors(f):
