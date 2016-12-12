5cd9b15d975f5820a01c37a04dd1bdec6ccc4554
jmtaysom <jmtaysom@users.noreply.github.com>
2016-12-12 11:39:15 -0500

    cleaned up prime_gen

--- Euler/Project003.py
+++ Euler/Project003.py
@@ -9,31 +9,23 @@ from math import sqrt
 
 
 def primes_gen():
-    counter = 0
     primes = [2,3,5]
     n = 7
     increment = [4,2,4,2,4,6,2,6]
     
     while True:
-        if counter == 0:
-            yield 2
-            counter += 1
-        elif counter == 1:
-            yield 3
-            counter += 1
-        elif counter == 2:
-            yield 5
-            counter += 1
+        yield 2
+        yield 3
+        yield 5
+        for i in primes[3:int(sqrt(n))]:
+            if n % i == 0:
+                break
         else:
-            for i in primes[3:int(sqrt(n))]:
-                if n % i == 0:
-                    break
-            else:
-                primes.append(n)
-                yield primes[-1]
-            a = increment.pop(0)
-            increment.append(a)
-            n += a
+            primes.append(n)
+            yield primes[-1]
+        a = increment.pop(0)
+        increment.append(a)
+        n += a
 
 
 def prime_factors(f):
