5633ad8db8ed6dd389856205dde428986f04c234
jmtaysom <james.taysom@gmail.com>
2016-12-13 15:18:25 -0500

    added better solution for 4

---
+++ Euler/004ep.py
@@ -0,0 +1,21 @@
+from math import sqrt
+
+def palindrome():
+    """
+    A palindromic number reads the same both ways.
+    The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+    Find the largest palindrome made from the product of two 3-digit numbers.
+    """
+    for x in range(999,100,-1):
+        pal = int(str(x) + str(x)[::-1])
+        for i in range(999,int(sqrt(pal)), -1):
+            if pal % i == 0 and len(str(i)) > 2 and len(str(pal/i)) > 2:
+                return pal
+
+
+if __name__ == '__main__':
+    from time import time
+    start = time()
+    print(palindrome())
+    print(time()-start)
