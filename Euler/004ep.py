8b36ee8ba61d0e3ceedfa1e8139987fdc449bcb3
jmtaysom <james.taysom@gmail.com>
2016-12-15 09:32:28 -0500

    updated 4 and 5

--- Euler/004ep.py
+++ Euler/004ep.py
@@ -1,17 +1,17 @@
 from math import sqrt
-
 def palindrome():
     """
     A palindromic number reads the same both ways.
-    The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-
-    Find the largest palindrome made from the product of two 3-digit numbers.
+    The largest palindrome made from the product 
+    of two 2-digit numbers is 9009 = 91 × 99.
+    Find the largest palindrome made from the 
+    product of two 3-digit numbers.
     """
     for x in range(999,100,-1):
         pal = int(str(x) + str(x)[::-1])
         for i in range(999,int(sqrt(pal)), -1):
             if pal % i == 0 and len(str(i)) > 2 and len(str(pal/i)) > 2:
-                return pal
+                return pal, i , pal / i
 
 
 if __name__ == '__main__':
--- Euler/Project005.py
+++ Euler/Project005.py
@@ -4,16 +4,54 @@ Created on Mon Jun 29 18:57:07 2015
 
 @author: jmtaysom
 """
-dividers = range(1,20)
+from time import time
+from collections import Counter
+from math import sqrt
+t = time()
 
-divided = True
-  
-start = 10  
-while divided:
-    for i in dividers:
-        if start % i != 0:
-            start += 10
-            break
-    else:
-        divided = False
-        print start
+def primes_gen():
+    primes = [2,3,5]
+    n = 7
+    increment = [4,2,4,2,4,6,2,6]
+    yield 2
+    yield 3
+    yield 5
+    while True:
+        for i in primes[3:int(sqrt(n))]:
+            if n % i == 0:
+                break
+        else:
+            primes.append(n)
+            yield primes[-1]
+        a = increment.pop(0)
+        increment.append(a)
+        n += a
+
+
+def prime_factors(x,primes):
+    pf = Counter()
+    while x > 1:
+        for prime in primes:
+            if x % prime == 0:
+                pf[prime] +=1
+                x /= prime
+    return pf
+
+
+def smallest_multiple(n):
+    primer = primes_gen()
+    primes = [next(primer)]
+    while max(primes) < n:
+        primes.append(next(primer))
+
+    pf = Counter()
+    for x in range(2, n+1):
+
+        pf = pf | prime_factors(x,primes)
+    total = 1
+    for k,v in pf.items():
+        total *= k**v
+    return total
+
+print(smallest_multiple(20))
+print(time() - t)
